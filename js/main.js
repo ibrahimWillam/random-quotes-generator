ccdf7aae7056f54c2318bde48298c1790d9bdd56
ibrahimWillam <ibrahimwillam62@gmail.com>
2023-12-14 00:30:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function texts(){
+    var text = ["“Be yourself; everyone else is already taken.”" , "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”","“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”","“So many books, so little time.”","“A room without books is like a body without a soul.”","“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”"];
+var auther =["― Oscar Wilde","― Marilyn Monroe","― Albert Einstein","― Frank Zappa","― Marcus Tullius Cicero","― Bernard M. Baruch"];
+var num= Math.floor(Math.random()*text.length);
+document.getElementById('text').innerHTML= text[num];
+document.getElementById('auther').innerHTML= auther[num];
+};
